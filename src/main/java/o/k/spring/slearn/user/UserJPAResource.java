29ebe9f786faa8fd54b6e72cfaa3bdda59a882c2
kirankumarmaili <kirankumar.maili@gmail.com>
2019-03-05 09:22:24 +0530

    Requesting mapping to the class and default unmarshalling

--- src/main/java/o/k/spring/slearn/user/UserJPAResource.java
+++ src/main/java/o/k/spring/slearn/user/UserJPAResource.java
@@ -2,13 +2,13 @@ package o.k.spring.slearn.user;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,36 +26,36 @@ import o.k.spring.slearn.exceptions.UserIdNotFoundException;
  * UserResource
  */
 @RestController
-@RequestMapping("jpa")
+@RequestMapping("jpa/users")
 public class UserJPAResource {
     
     @Autowired
     private UserRepository userService;
 
-    @GetMapping(value="/users")
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
-    @GetMapping(value="/users/{id}")
+    @GetMapping(value="/{id}",produces={MediaType.APPLICATION_JSON_VALUE})
     public Resource<User> getUserWithId(@PathVariable int id) {
-    	User user = Optional.of(userService.getOne(id)).orElseThrow(() -> new UserIdNotFoundException(id));
+    	User user = userService.findById(id).orElseThrow(() -> new UserIdNotFoundException(id));
     	Resource<User> resource = new Resource<>(user);
     	ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).getAllUsers());
     	resource.add(linkTo.withRel("all-users"));
         return resource;
     }
 
-    @PostMapping(value="/users")
+    @PostMapping
     public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
         User savedUser = userService.save(user);
         URI locationOfNewUser = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(locationOfNewUser).build();
     }
     
-    @DeleteMapping(value="/users/{id}")
+    @DeleteMapping(value="/{id}")
     public User deleteUser(@PathVariable int id) {
-    	User user = Optional.of(userService.getOne(id)).orElseThrow(() -> new UserIdNotFoundException(id));
+    	User user = userService.findById(id).orElseThrow(() -> new UserIdNotFoundException(id));
     	userService.deleteById(id);
     	return user;
     }
--- src/main/java/o/k/spring/slearn/user/UserResource.java
+++ src/main/java/o/k/spring/slearn/user/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,17 +25,18 @@ import o.k.spring.slearn.exceptions.UserIdNotFoundException;
  * UserResource
  */
 @RestController
+@RequestMapping(value="/users")
 public class UserResource {
     
     @Autowired
     private UserDaoService userService;
 
-    @GetMapping(value="/users")
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
-    @GetMapping(value="/users/{id}")
+    @GetMapping(value="/{id}")
     public Resource<User> getUserWithId(@PathVariable int id) {
     	User user = userService.findOne(id).orElseThrow(() -> new UserIdNotFoundException(id));
     	Resource<User> resource = new Resource<>(user);
@@ -43,14 +45,14 @@ public class UserResource {
         return resource;
     }
 
-    @PostMapping(value="/users")
+    @PostMapping
     public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
         User savedUser = userService.save(user);
         URI locationOfNewUser = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(locationOfNewUser).build();
     }
     
-    @DeleteMapping(value="/users/{id}")
+    @DeleteMapping(value="/{id}")
     public User deleteUser(@PathVariable int id) {
     	return userService.deleteById(id).orElseThrow(() -> new UserIdNotFoundException(id));
     }
