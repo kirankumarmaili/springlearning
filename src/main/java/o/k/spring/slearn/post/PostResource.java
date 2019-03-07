747a9fbe5bfcdefb4feb2bc7f697381e98d18896
kirankumarmaili <kirankumar.maili@gmail.com>
2019-03-07 16:34:39 +0530

    Post Resource changes

--- src/main/java/o/k/spring/slearn/post/PostResource.java
+++ src/main/java/o/k/spring/slearn/post/PostResource.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import o.k.spring.slearn.exceptions.UserIdNotFoundException;
+import o.k.spring.slearn.user.User;
+import o.k.spring.slearn.user.UserRepository;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -14,8 +21,20 @@ public class PostResource {
 	@Autowired
 	PostDaoService postDaoService;
 	
+	@Autowired
+	UserRepository userRespository;
+	
 	@GetMapping
 	public List<Post> getAllPosts() {
 		return postDaoService.findAll();
+	}
+	
+	@PostMapping(value="{userid}")
+	public void addPostFromUser(@PathVariable("userid") Integer userid, @RequestBody Post post) {
+		User user = userRespository.findById(userid).orElseThrow(() -> new UserIdNotFoundException(userid));
+		if(post.getId()!=null) {
+			post.setId(null);
+		}
+		post.setUser(user);
 	}
 }
